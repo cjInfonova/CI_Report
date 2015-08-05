66842c27ba444e8f3a85df7b53a30f92756ab192
cjInfonova <christian.jahrbacher@infonova.com>
2015-08-05 14:25:18 +0200

    asdf

--- Auto_Repo_Plugin/src/test/java/com/infonova/jenkins/FailureBuilderUTest.java
+++ Auto_Repo_Plugin/src/test/java/com/infonova/jenkins/FailureBuilderUTest.java
@@ -3,5 +3,46 @@ package com.infonova.jenkins;
 /**
  * Created by dominic.gross on 03.08.2015.
  */
-public class FailureBuilderUTest {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.infonova.jenkins.*;
+import org.easymock.EasyMockSupport;
+import org.json.JSONException;
+import org.junit.Before;
+import org.junit.Test;
+import static org.easymock.EasyMock.*;
+import java.io.IOException;
+import java.util.Arrays;
+
+
+
+public class FailureBuilderUTest extends EasyMockSupport {
+    private JenkinsAccess jenkinsAccess;
+    private final static String standardUrl ="";
+    private FailureBuilder failureBuilder;
+
+    @Before
+    public void setup(){
+        jenkinsAccess=createMock(JenkinsAccess.class);
+        failureBuilder = new FailureBuilder(jenkinsAccess, Arrays.asList("A1ON-java-build-trunk"),standardUrl);
+    }
+
+    @Test
+    public void testFailureBuilder() throws IOException, JenkinsException {
+        JsonNode jn = new ObjectMapper().readTree("{\"duration\":1058.291,\"empty\":false,\"failCount\":0,\"passCount\":31,\"skipCount\":1,\"suites\":[{\"cases\":[{\"age\":1,\"className\":\"com.infonova.jtf.ta.generic.kundenverwaltung.CustomerAndServiceLockUnlockTest\",\"duration\":54.998,\"errorDetails\":\"Test\",\"errorStackTrace\":\"TestStack\",\"failedSince\":0,\"name\":\"lockAndUnlockUser\",\"skipped\":false,\"skippedMessage\":null,\"status\":\"FAILED\",\"stderr\":null,\"stdout\":null},{\"age\":1,\"className\":\"com.infonova.jtf.ta.generic\",\"duration\":54.998,\"errorDetails\":null,\"errorStackTrace\":\"TestStack\",\"failedSince\":0,\"name\":\"lockUnlockUser\",\"skipped\":false,\"skippedMessage\":null,\"status\":\"FAILED\",\"stderr\":null,\"stdout\":null},{\"age\":0,\"className\":\"com.infonova.jtf.ta.generic.kundenverwaltung.CustomerAndServiceLockUnlockTest\",\"duration\":54.998,\"errorDetails\":null,\"errorStackTrace\":null,\"failedSince\":0,\"name\":\"lockAndUser\",\"skipped\":false,\"skippedMessage\":null,\"status\":\"PASSED\",\"stderr\":null,\"stdout\":null}],\"duration\":85.029,\"id\":null,\"name\":\"com.infonova.jtf.ta.generic.kundenverwaltung.CustomerAndServiceLockUnlockTest\",\"stderr\":null,\"stdout\":null,\"timestamp\":null}]}");
+        JsonNode jn2 = new ObjectMapper().readTree("{\"failCount\":0,\"skipCount\":1,\"totalCount\":32,\"urlName\":\"testReport\",\"childReports\":[{\"child\":{\"number\":21,\"url\":\"https://ci.infonova.at/job/A1OpenNet/job/A1ON-jtf-livetests-trunk12c/com.bearingpoint.ta$opennet-jtf-tests/21/\"},\"result\":{\"duration\":1058.291,\"empty\":false,\"failCount\":0,\"passCount\":31,\"skipCount\":1,\"suites\":[{\"cases\":[{\"age\":0,\"className\":\"com.infonova.jtf.ta.generic.kundenverwaltung.AonAccountManagementTest\",\"duration\":3.802,\"failedSince\":0,\"name\":\"testGetProductsForAonAccount\",\"skipped\":false,\"status\":\"PASSED\"},{\"age\":0,\"className\":\"com.infonova.jtf.ta.generic.kundenverwaltung.AonAccountManagementTest\",\"duration\":1.302,\"failedSince\":0,\"name\":\"testGetAonAccounts\",\"skipped\":false,\"status\":\"FAILED\"}],\"duration\":5.104,\"id\":null,\"name\":\"com.infonova.jtf.ta.generic.kundenverwaltung.AonAccountManagementTest\",\"timestamp\":null}]}}]}");
+        try {
+            expect(jenkinsAccess.getJsonNodeFromUrl(anyObject(String.class))).andReturn(jn2);
+            expect(jenkinsAccess.getJsonNodeFromUrl(anyObject(String.class))).andReturn(jn).anyTimes();
+        } catch (JSONException jsexe) {
+            jsexe.printStackTrace();
+        } catch (IOException exe) {
+            exe.printStackTrace();
+        } catch (JenkinsException jex) {
+            jex.printStackTrace();
+        }
+        replayAll();
+        failureBuilder.readErrors();
+        verifyAll();
+    }
 }
--- Auto_Repo_Plugin/src/test/java/com/infonova/jenkins/HTML_GeneratorUTest.java
+++ Auto_Repo_Plugin/src/test/java/com/infonova/jenkins/HTML_GeneratorUTest.java
@@ -3,5 +3,62 @@ package com.infonova.jenkins;
 /**
  * Created by dominic.gross on 31.07.2015.
  */
-public class HTML_GeneratorUTest {
+
+import com.infonova.jenkins.*;
+import org.easymock.EasyMockSupport;
+import org.json.JSONException;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.easymock.EasyMock.*;
+
+
+public class HTML_GeneratorUTest extends EasyMockSupport {
+
+    public HTMLGenerator htmlgen;
+    public final static List<Job> jobList = Arrays.asList(new Job("Test1", "FAILED", 1, 2, "no"), new Job("Test2", "PASSED", 0, 2, "no"),new Job("Test2", "SKIPPED", 0, 2, "no"));
+    public final static List<Failure> failList = Arrays.asList(new Failure(1, "class1", "fail all", "ff", "FAILED", "Test1"), new Failure(0, "class2", "", "", "PASSED", "Test2"), new Failure(0, "class3", "", "", "PASSED", "Test2"));
+
+
+    @Before
+    public void Setup() {
+        htmlgen = new HTMLGenerator();
+    }
+
+    @Test
+    public void testPreCode() throws IOException {
+        BufferedWriter bwr = new BufferedWriter(new FileWriter(new File("test.html")));
+
+        replayAll();
+        htmlgen.staticPreCode(bwr);
+        verifyAll();
+
+    }
+
+    @Test
+    public void testBuildTable() throws IOException {
+        BufferedWriter bwr = new BufferedWriter(new FileWriter(new File("test.html")));
+
+        replayAll();
+        htmlgen.buildTable(jobList, bwr, "Tab1", failList);
+        verifyAll();
+
+    }
+
+
+    @Test
+    public void testPostCode() throws IOException {
+        BufferedWriter bwr = new BufferedWriter(new FileWriter(new File("test.html")));
+
+        replayAll();
+        htmlgen.staticPostCode(bwr, jobList, failList, "link1", "link2");
+        verifyAll();
+    }
 }
