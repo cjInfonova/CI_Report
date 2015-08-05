8bf1bc2444527d2b1b809c882f952765500eebbf
grodoa11 <grodoa11@htlkaindorf.at>
2015-08-05 08:00:16 +0200

    3 Tests

--- Auto_Repo_Plugin/src/test/java/com/infonova/jenkins/ReportMojoUTest.java
+++ Auto_Repo_Plugin/src/test/java/com/infonova/jenkins/ReportMojoUTest.java
@@ -2,38 +2,46 @@ package com.infonova.jenkins;
 
 //import com.bearingpoint.jbpm.TestExecutionContext;
 import com.infonova.jenkins.*;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.easymock.EasyMockSupport;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by dominic.gross on 21.07.2015.
  */
 
-public class ReportMojoUTest {
+public class ReportMojoUTest extends EasyMockSupport{
 
     // @Parameter(defaultValue = "christian.jahrbacher")
-    private static String username = "christian.jahrbacher";
+    private final static String username = "christian.jahrbacher";
     // @Parameter(defaultValue = "Cj170615!")
-    private static String password = "Cj170615!";
+    private final static String password = "Cj170615!";
     // @Parameter (defaultValue = "https://ci.infonova.at")
-    public static String JENKINS_URL = "https://ci.infonova.at";
+    public final static String JENKINS_URL = "https://ci.infonova.at";
     // @Parameter
-    public static String jobname = "A1OpenNet";
-    // @Parameter
-    private static List<JenkinsSystem> jenkinsSystemList;
+    public final static String jobname = "A1OpenNet";
     // @Parameter(defaultValue = "dd.MM.yyyy")
-    private static SimpleDateFormat dateformat = new SimpleDateFormat("dd.MM.yyyy");
+    private final static SimpleDateFormat dateformat = new SimpleDateFormat("dd.MM.yyyy");
+
     @Test
-    public void execute() throws IOException {
+    public void execute()throws MojoExecutionException, MojoFailureException {
+        List<JenkinsSystem> jenkinsSystemList = new ArrayList<JenkinsSystem>();
         Usersettings us = new Usersettings(username, password);
         JenkinsAccess jenkinsAccess = new JenkinsAccess(JENKINS_URL, us.getUsername(), us.getPassword());
         JobBuilder jobBuilder = new JobBuilder(jenkinsAccess, JENKINS_URL+"/job/"+jobname+"/job/", dateformat);
         HTMLGenerator htmlgen = new HTMLGenerator();
         DataAccessLayer dal = new DataAccessLayer(jenkinsAccess, JENKINS_URL, jobname, dateformat, jobBuilder, htmlgen);
-        dal.startBuildingReport();
+        ReportMojo repo = new ReportMojo();
+
+        replayAll();
+        repo.execute();
+        verifyAll();
 
     }
 
